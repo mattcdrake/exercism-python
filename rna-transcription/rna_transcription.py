7d33319ed905c0897f91fc0e8f8268ddbf78a0b0
Matt Drake <drake.mattc@gmail.com>
2018-03-23 17:34:19 -0400

    finished RNA

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,14 @@
 def to_rna(dna_strand):
-    pass
+    out_dna = ""
+    for e in dna_strand:
+        if e == "G":
+            out_dna += "C"
+        elif e == "C":
+            out_dna += "G"
+        elif e == "T":
+            out_dna += "A"
+        elif e == "A":
+            out_dna += "U"
+        else:
+            raise ValueError("Non-RNA character supplied")
+    return out_dna
