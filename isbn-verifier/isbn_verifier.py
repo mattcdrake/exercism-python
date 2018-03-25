58d8e8ab3bf2fda9aea87b8b9defb4e2237e90e8
Matt Drake <drake.mattc@gmail.com>
2018-03-24 21:36:37 -0400

    Finished ISBN

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,22 @@
+import re
+
 def verify(isbn):
-    pass
+    formattedString = re.sub('[^xX0-9]+', '', isbn)
+
+    if (len(formattedString) != 10):
+        return False
+
+    multiplicand = 10
+    sumDigits = 0
+
+    for char in formattedString:
+        if (multiplicand != 1 and (char == 'x' or char == 'X')):
+            return False
+        elif (multiplicand == 1 and (char == 'x' or char == 'X')):
+            sumDigits += 10
+        else:
+            sumDigits += multiplicand * int(char)
+        
+        multiplicand -= 1
+    
+    return sumDigits % 11 == 0
