5d56c326a60e9ed99a7638f3b356b4b947c6ec3c
Matt Drake <drake.mattc@gmail.com>
2018-03-25 16:24:31 -0400

    Finished Hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+    if (len(strand_a) != len(strand_b)):
+        raise ValueError("DNA strands are of different length")
+    
+    distance = 0
+    index = 0
+
+    while (index < len(strand_a)):
+        if strand_a[index] != strand_b[index]:
+            distance += 1
+        index += 1
+    
+    return distance
