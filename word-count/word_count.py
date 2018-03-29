99be91828f0f449f90007834296abe32b882e921
Matt Drake <drake.mattc@gmail.com>
2018-03-28 20:35:15 -0400

    Finished word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -4,15 +4,16 @@ def word_count(phrase):
     mod_phrase = phrase.lower()
     words = re.findall(r"([a-z0-9\']+)", mod_phrase)
 
-    for word in words:
-        word = word.strip("'")
+    print(words)
 
     word_count = {}
 
     for word in words:
+        word = word.strip("'")
+
         if word in word_count:
             word_count[word] += 1
         else:
             word_count[word] = 1
-    
+
     return word_count
