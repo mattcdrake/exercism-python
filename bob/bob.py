150b2e4998c432f4dfa3768ddf15e802480c5969
Matt Drake <drake.mattc@gmail.com>
2018-04-03 10:51:06 -0400

    Finished Bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,14 @@
+import re
+
 def hey(phrase):
-    pass
+    phrase = phrase.strip()
+    if (len(phrase) <= 0):
+        return "Fine. Be that way!"
+    elif (re.search('[a-zA-Z]', phrase) and phrase.upper() == phrase and 
+        phrase[-1] == "?"):
+        return "Calm down, I know what I'm doing!"
+    elif (phrase[-1] == "?"):
+        return "Sure."
+    elif (re.search('[a-zA-Z]', phrase) and phrase.upper() == phrase):
+        return "Whoa, chill out!"
+    return "Whatever."
