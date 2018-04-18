d9584c4d0b7648324ba84a11e8964b24fea15974
Matt Drake <drake.mattc@gmail.com>
2018-04-18 18:11:08 -0400

    Finished armstrong numbers

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,11 @@
 def is_armstrong(number):
-    pass
+    temp_num = number
+    digits = len(str(number))
+    sum_digs = 0
+
+    for i in range (0, digits):
+        temp_digit = temp_num % 10
+        sum_digs += temp_digit ** digits
+        temp_num //= 10
+
+    return sum_digs == number
