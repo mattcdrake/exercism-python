e875dcdad573fe78079e08f389297f2e932dad93
mattcdrake <drake.mattc@gmail.com>
2018-04-22 20:37:33 -0400

    finished diff of squares

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,20 @@
 def square_of_sum(count):
-    pass
+    square = 0
+
+    for i in range(1, count + 1):
+        square += i
+    
+    return square ** 2
 
 
 def sum_of_squares(count):
-    pass
+    sum_squares = 0
+
+    for i in range(1, count + 1):
+        sum_squares += i**2
+    
+    return sum_squares
 
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)
